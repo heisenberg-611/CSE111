328700d4a7393c76dfaf665ec7a554ac4b967de5
dhrubojyoti <lnsaha1214@gmail.com>
2024-09-18 11:44:47 +0600

    18/09/24

---
+++ Lab 2/Task8.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Task8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        
+        int prime_count = 0;
+        
+        for (int i = start; i <= end; i++) {
+            if (i <= 1) {
+                continue;
+            }
+            
+            boolean primeCntr = true;
+            for (int j = 2; j <= i/2; j++) {
+                if (i % j == 0) {
+                    primeCntr = false;
+                    break;
+                }
+            }
+            if (primeCntr) {
+                prime_count++;
+            }
+        }
+        System.out.println("There are " + prime_count + " prime numbers between " + start + " and " + end);
+    }
+}
